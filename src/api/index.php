4539282be1f7a2b41840d5111784432d60348c29
Maria Petersen <mau.petersen@gmail.com>
2023-06-20 11:50:51 +0200

    adding php server test

--- src/api/index.php
+++ src/api/index.php
@@ -1,17 +1,22 @@
 <?php 
     header("Access-Control-Allow-Origin: *");
+    header("Access-Control-Allow-Headers: *"); 
     $rest_json = file_get_contents("php://input");
     $_POST = json_decode($rest_json, true);
 
-    if (!isset($_POST['name']) || !isset($_POST['email'])) die();
+    if (!isset($_POST['name'])) die();
 
+    if($_POST){
+        http_response_code(200);
+        echo $_POST['name'];
+    }
     if($_POST){
         http_response_code(200);
         $subject = $_POST['name'];
         $to = "mau.petersen@gmail.com";
         $from = $_POST['email'];
 
-        $message = $_POST['number'] . $_POST['message'];
+        $message = $_POST['name'];
 
         $headers = "MIME-Version: 1.0\r\n";
         $headers.= "Content-type: text/html; charset=UTF-8\r\n";
--- src/pages/Inscription.js
+++ src/pages/Inscription.js
@@ -3,7 +3,7 @@ import "./styles.scss"
 import Header from '../components/Header/Header'
 import PageTitle from '../components/TitleComponents/PageTitle/PageTitle';
 import Textbox from "../components/Textbox/Textbox";
-import Forms from "./Forms";
+import Forms from "../components/Forms/Forms";
 import { Outlet } from "react-router-dom";
 
 const Inscription = () => {
